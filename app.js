4251392afd2193982107edd6600b159c94c66cd1
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-03-27 04:12:14 +0500

    Chapter 01 to 20.

---
+++ app.js
@@ -0,0 +1,933 @@
+
+// // Chapter 01  { Alert }.........................................................
+
+// // Tast 01
+
+// //Alert thes following (individually)
+
+// // var a = "First Name (Anas )" 
+// // alert(a)
+// // var b = "Last Name (Raza)" 
+// // alert(b)
+// // var c = "Email (anusm9722@gmail.com)"
+// // alert(c)
+// // var d = "Phone Number (8426594215496)"
+// // alert(d)
+// // var e = "Password (26-49865468456)"
+// // alert(e)
+
+// // Chapter 02  { Variable for string }..............................................
+
+// //Task 01
+
+// // var currectCase = ""
+
+// // //Task 02
+
+// // var CurrectCase = "Hello world"
+
+// // Task 03
+
+// // var teamName = "Anassquad"
+
+// // alert(teamName)
+
+// //Task 04
+
+// // var bestMane =  "Hamid"
+
+// // Chapter 03  { Variables of numbers }..............................................
+
+// // Task 01
+
+// // var casQty = ""
+
+// //Task 02
+
+// // var caseQty = 144;
+
+// // Task 03
+
+// // var num = "9";
+
+// //Task 04
+
+// // var num1 = 9;
+// // var num2 = 3;
+
+// // var newNumber = num1 + num2;
+
+// // Task 05
+
+// // var merchTotal = 100;
+// // var shippingCharge = 10;
+
+// // var orderTotale = merchTotal + shippingCharge;
+
+// // console.log(orderTotale)
+
+// // Task 06
+
+// // var petrolCharges = 300;
+
+// // var totalCharges = petrolCharges + 50;
+
+// // console.log(totalCharges)
+
+
+// // Chapter 04 { Variables of numbers }..............................................
+
+// // Task 01
+
+// // var productcost = 3.45;
+
+// // Task 02
+
+// // var nameOfband 
+
+// //Task 03
+
+// // var amoung12 = 39;
+
+// // Task 04
+
+// // var anasRaza = "";
+
+// // Task 05
+
+// // Legal
+
+// // var asd = '' //legal
+// // var ab@s = '' //legal
+// // var as12 '' //legal
+
+// // illegal
+
+// // var ab_ = '' //illegal
+// // var 12sd = '' //ilegal
+// // var var = '' //illegal
+// // var 123 = '' //ilegal
+// // var _ = '' //illegal
+
+
+// // Chapter 05 { Math Expression 1}..............................................
+
+// // Task 01
+
+// // What is the name and symbol of the arithmetic operator that 
+// // gives you the remainder when one number is divided by another?
+
+// // /	Division
+
+// // ..................................................................................
+
+// // Arithmetic Operators
+
+
+// // Operator	Description
+// // +	Addition
+// // -	Subtraction
+// // *	Multiplication
+// // **	Exponentiation
+// // /	Division
+// // %	Modulus 
+// // ++	Increment
+// // --	Decrement
+
+// // Task 02
+
+// // var num = 20%6;
+
+// // console.log(num) {{answe is  (2)}}
+
+// // Task 03
+
+// // var number = 1000 * 2000;
+
+// // console.log(number)
+
+// // var largNum = 2000000;
+
+// // Task 04
+
+// // var firstNum = 50;
+// // var secNum = 20;
+
+// // var result = firstNum - secNum;
+// // console.log(result)
+
+// // Task 05
+
+// //  var dividedNum = 20/2;
+
+// //  console.log(dividedNum)
+
+// // Task 06
+
+// // var multiNum = 20 * 3;
+
+// // alert(multiNum)
+
+
+// // Chapter 06 { Math Expression 2}..............................................
+
+// // Task 01
+
+// // Code a short form of x = x + 1; Use either of the two legal 
+// // // expressions.
+
+// // 1st
+
+// // var num1 = 2;
+
+// // num1 += 2
+
+// // console.log(num1)
+
+// // 2nd
+
+// // var num2 = 2;
+
+// //var inc = num2++ 
+
+// // console.log(num2)
+
+// // Task 02
+
+// // If x has a value of 100, what is the fastest way to reduce it to 99 
+// // with a math expression?
+
+// // var num = 100;
+
+// // var y = --num;
+
+// // console.log(y)
+
+// // Task 03
+
+// // var x = 50;
+// // var y = x++;
+
+// // console.log(y)
+
+// // y value is 50
+
+// // Task 04
+
+// // var y = 50;
+// // var z = --y;
+
+// // console.log(z)
+
+// // y value is 49
+
+// // Task 05
+
+// // var num = 10;
+
+// // var newNum = --num;
+
+// // console.log(newNum)
+
+// // Task 06
+
+// // var num = 5;
+
+// // var addNum = ++num;
+
+// // console.log(addNum) 
+
+// //real value is (5)
+
+// // var realNum = 5;
+
+// // Task 07
+
+// // var num = 44;
+
+// // var incNum = ++num;
+
+// // alert(incNum);
+
+// // Chapter 07 { Math Expression 3}..............................................
+
+// // Task 01
+
+// // var calculatedNum = 2 + ( 2 * 6 );
+
+// // //calculatedNum value is (  14  )
+
+// // Task 02
+
+// // var calculatedNum = (2 + 2) * 6;
+
+// // calculatedNum value is (  24  )
+
+// // Task 03
+
+// // var calculatedNum = (2 + 2) * (4 + 2);
+
+// // calculatedNum value is (  24  )
+
+
+// // Task 04
+
+// // var calculatedNum = ((2 + 2) * 4) + 2;
+
+// // calculatedNum value is (  18  )
+
+// // Task 05
+
+// // var calculatedNum =  2 + 2 * 4 + 10;
+
+
+// // calculatedNum value is (   )
+
+// // Task 06
+
+// // var calculatedNum = 2 + 2 * 4 + 10;
+
+// // calculatedNum value is (  20  )
+
+// // Task 07
+
+// // var calculatedNum =  4 / 2 * 4;
+
+// // console.log(calculatedNum)
+
+// // calculatedNum value is (  8  )
+
+// // Chapter 08 (Concatenating Text Strings)..............................................
+
+// // Task 01 
+
+// // var num = "2" + "2";
+
+// // num value is (  22  )
+
+// // Task 02
+
+// // message = ("Hello," + "Dolly");
+
+// // alert(message)
+
+// // message value is ( Hello,Dolly  )
+
+// // Task 03
+
+// // alert("33" + 3);
+
+// // alaert value is ( 333 )
+
+// // Task 04
+
+// // var names = ("pakistan," + "zindabad")
+
+// // alert(names)
+
+// // Task 05
+
+// // var numString = ("Aman," + 123);
+
+// // console.log(numString)
+
+// // Task 06
+
+// // var first = ("ANAS")
+// // var sec = ("RAZA")
+
+// // var result = first + sec;
+
+// // console.log(result)
+
+
+// // Chapter 09 { Prompts }..............................................
+
+
+// //Task 01
+
+// // var firstName = prompt("Enter first name");
+
+// // console.log(firstName)
+
+// //Task 02
+
+// // var option = "Country";
+
+// // var defaultAnswer = "China";
+
+// // var result = prompt( option, defaultAnswer)
+
+// //Task 03
+
+// // var yourName = prompt("Enter Your Name");
+
+// //Task 04
+
+// // var question = "What's the name of last prophit"
+
+// // var defaultAnswer = "Hazrat Muhammad Mustafa"
+
+// // var result = prompt( question, defaultAnswer);
+
+// //Task 05
+
+// // var first = "who are you";
+
+// // var Sec = "Im AI";
+
+// // var third = "";
+
+// // var result = prompt(first,Sec,third);
+
+// //Task 06
+
+// // var fullName = "Enter your Fullname"
+
+// // var defaultAnswer = "Muhammad Anas Raza"
+
+// // alert(prompt(fullName,defaultAnswer))
+
+
+// // Chapter 10 (if statments).................................................
+
+// // Task 01
+
+// // var city = "Karachi";
+
+// // if(city = "Karachi"){
+// //     console.log("THE CITY OF LIGHTS")
+// // }
+
+// // Task 02
+
+// // var value = "y"
+
+// // if(value = "y"){
+// //     console.log(true)
+// // }
+
+// // Task 03
+
+// // var zipcode = 10010;
+
+// // if(zipcode = 10010){
+// //     alert("Karachi")
+// // }else{
+// //     alert("Please write correct city ")
+// // }
+
+// // Task 04
+
+// // var x = 5;
+
+// // if (x === 5) {
+
+// //     console.log(10);
+// // }
+
+
+// // Chapter 11 (Comparison Operators)..........................
+
+// // Task 01
+
+// // var first = "anus"
+
+// // if(first === "anus"){
+// //     console.log("hi mr anus")
+// // }
+
+// // Task 02
+
+// // var num1 = 100;
+
+// // var num2 = 99;
+
+// // if(num1 >= num2){
+
+// //     console.log("well done bro")
+// // }
+
+// // Task 03
+
+// // var num1 = 50;
+
+// // var num2 = 50;
+
+// // if(num1 == num2){
+// //     console.log("50")
+// // }
+
+// // Task 04
+
+// // var marks = 100;
+
+// // if(marks == 100){
+// //     alert("Congratulations")
+// // }
+
+// // Task 05
+
+// // var firstName = prompt("First Name")
+
+// // var name1 = "Anus Raza"
+
+// // if(firstName == name1){
+// //     alert("No match")
+// // }
+
+// // Chapter 12  (if…else and else if statements).........................
+
+// //Task 01
+
+// // var requared = 23;
+
+
+// // if(requared >= 33){
+// //     alert("Very good bacha")
+// // }else(alert("Koi nhi next time"));
+
+// //Task 02
+
+// // var marks = prompt("Enter your marks")
+
+// // if(marks >= 33){
+// //     console.log("C")
+// // }else if(marks >= 66){
+// //     console.log("B")
+// // }else if(marks >= 75){
+// //     console.log("A")
+// // }else{
+// //     console.log("Fail")
+// // }
+
+// //Task 03
+
+// // var name1 = "kashif"
+// // if (name1 === "Anas") {
+// //     alert("Anas");
+// //    }else{
+// //     alert(name1)
+// //    }
+
+// //Task 04
+
+// // var city = prompt("Enter your city")
+
+// // if(city == "Karachi"){
+// //     alert("acknowledging it is Karachi")
+// // }else if( city == "Lahore"){
+// //     alert("acknowledging it is Lahore")
+// // }else{alert(city)}
+
+
+
+// // Chapter 13   (Testing sets of conditions).........................
+
+// //Task 01
+
+// // var a = "gaol"
+// // var b = "gaol"
+// // var c = "gaol"
+// // var d = "gaol"
+
+// // if(a === b){
+// //     console.log("a same as b")
+// // }
+// // if(c === d){
+// //     console.log("c same as d")
+// // }
+
+// //Task 02
+
+// // var a = "asd"
+// // var b = "asd"
+
+// // var c = "llc"
+// // var d = "abc"
+
+// // if(a === b || c === d){
+// //     console.log("a same as but b not same as d")
+// // }
+
+// //Task 03
+
+// // var name1 = "Hamza"
+// // var name2 = "Arsalan"
+// // var age = "23"
+
+// // if(name1 === "Hamza" || name2 === "Arsalan" && age < 60){
+// //     console.log("You are eligible")
+// // }
+
+// //Task 04
+
+// // var num1 = 20;
+// // var num2 = 40;
+
+// // if ( num1 <= num2 ){
+// //     alert("This is less ")
+// // }else if( num1 >= num2 ){
+// //     alert("This is Greater")
+// // }
+
+// //Task 05
+
+// // var firstName = prompt("Ente first Name") 
+// // var lastName = prompt("Enter last Name") 
+
+// // if(firstName === lastName){
+// //     alert("you are eligible")
+// // }
+
+
+// // Chapter 14 (If statements nested)...........................
+
+// // Task 01
+
+// // var Password = prompt("Enter password")
+
+// // if( Password !== "" ){
+// //     if(Password.length <= 5){
+// //         alert("password must be greater than 5")
+// //     }else{
+// //         alert("OK")
+// //     }
+// // }
+
+// //Task 02
+
+// // var countity = 2;
+
+// // var apple = "Red";
+
+// // if(countity === 2){
+// //     if(apple === "Red"){
+// //         alert("OK")
+// //     }
+// // }
+
+
+// //Task 03
+
+// // var a = 10;
+
+// // var b = 20;
+
+// // if (a === 1 || b === 20){
+// //     alert("ok")
+// // }
+
+// //Task 04
+
+// // var fNum = 33;
+// // var sNum = 33;
+
+// // if(fNum === sNum){
+// //     if(fNum <= sNum){
+// //         alert( "----")
+// //     }
+// // }
+
+// // Chapter 15 (Array I)......................
+
+// //Task 01
+
+// // var Array = ["",""]
+
+// //Task 02
+
+// // var Array = ["ANAS"]
+
+// //Task 03
+
+// // var alphabet = ["h","i","j","k"];
+
+// // alert( alphabet [2])
+
+// //Task 04
+
+// // var alphabet=["h","i","j","k", "l","m", "n", "o"]
+
+// // console.log(alphabet.length)
+
+// //TATAEL LENGTH IS (8)
+
+// //Task 05
+
+// // var w = ["anus"];
+
+// // w[1] = "qasim";
+
+// // console.log(w[1]);
+
+
+// // Chapter 16 (Array II).........................................
+
+// //Task 01
+
+// // var array = ['anas','hamza','kashif'];
+
+// // array.push = "raza";
+
+// // alert(array.push);
+
+
+// //Task 02
+
+// // var Alphabet=["h","i","j","k"];
+
+// // var a = Alphabet.pop(3);
+
+// // console.log(a,Alphabet)
+
+// //Task 03
+
+// // var Alphabet=["h","i","j","k"];
+
+// // var newNum = Alphabet.push[4] = "l";
+
+// // console.log(Alphabet,newNum)
+
+// // Chapter 16 (Array III).................................
+
+// //Task 01
+
+// // var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// // var remove = sizes.shift();
+
+// // console.log(remove,sizes)
+
+// //Task 02
+
+// // var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+
+// // var add = sizes.unshift(1,2,3);
+
+// // console.log(add, sizes)
+
+// //Task 03
+
+// // var array = ["firsteam"];
+
+
+// // array.unshift("newTeam");
+
+// // alert(array[0]);
+
+// //Task 04
+
+// // var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// // var index = sizes.indexOf("XL");
+
+// // sizes.splice(index, 0, "L");
+
+// // console.log(sizes);
+
+// //Task 05
+
+// // var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// // var copregsizesy = sizes.splice(0,3);
+
+// // console.log(sizes)
+
+// //Task 06
+
+// // var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+// // var dogIndex = pets.indexOf("dog");
+
+// // pets.splice(dogIndex + 1, 0, "parrot", "rabbit");
+
+
+// // pets.splice(dogIndex + 2, 3);
+
+// // console.log(pets);
+
+
+
+// //Task 07
+
+// // var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+// // var dogIndex = pets.indexOf("dog")
+
+// // pets.splice(dogIndex + 1,2)
+
+// // console.log(pets)
+
+// //Task 08
+
+// // var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+
+// // var reducedPets = pets.slice(3, 5);
+
+// // console.log(reducedPets);
+
+
+// // Chapter 17 - 20 (for Loops)................................
+
+// // Task 01
+
+// // for(var i = 0; i <=5; i++){
+//     //     console.log(i)
+//     // }
+
+// // Task 02
+
+// // for(var i = 0; i <= 12; i++){
+// //   console.log(i)
+// // }
+
+// //Task 03
+
+// // "(" ")" ";" ";"
+
+// // for var i = 0 i <= 4 i++   (this in incorrect)
+
+// // for(var i = 0; i <= 4; i++){
+
+// // }
+
+// //Task 04
+
+// // for( var counter = 0; counter < 100; counter++){
+// //     console.log(counter)
+// // }
+
+// //Task 05
+
+// for( var count = 6; count >0; count--){
+//     console.log(count)
+// }
+
+// //Task 06
+
+// var arr = ['20','76','54','34','54','33','44','40']
+
+// var numElem = arr.length;
+
+// console.log(numElem)
+
+// //Task 07
+
+// var flag = true;
+
+// console.log(flag)
+
+
+// //Task 08
+
+// var pets = ["dog","cat","fish","bird"];
+
+// for(var i = 0; i < pets.length; i++){
+//     console.log(("Pet of index"),i,":", pets[i])
+// }
+
+// //Task 09
+
+// for(var i = 0; i <= 10; i++){
+//     if(i===1){
+//         alert("it should be 1")
+//     }
+// }
+
+// //Task 10
+
+// var names = ["Ahmad","kashif",'kashan','areeb','sami','aryan'];
+
+// var firstName = prompt( "Enter first name" )
+
+// for(var i = 0; i < names.length; i++){
+
+//     if(names[i]===firstName){
+//        alert("Enter")
+//     }else{
+//         alert("Pleasewrite correct user name")
+//     }
+// }
+
+// //Task 11
+
+// var matchFound = false;
+
+// var list = ["bubble",'fish','tye']
+
+// var userInput = prompt();
+
+// for (var i = 0; i < list.length; i++) {
+//     if (userInput === list[i]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+// };
+
+// //Task 12
+
+// var firstArr = ["a", "b", "c", "d", "e", "f", "g"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+     
+//     for (var j = 0; j < secondArr.length; j++) {
+
+//         console.log(firstArr[i] + secondArr[j]);
+//     }
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
